2a78330efe12aece0ce950133a53c80add006e68
Fred <6550969+MagicFred888@users.noreply.github.com>
2025-01-02 09:20:34 +0100

    Solver for day 01

---
+++ AdventOfCode2017/Solver/Day01.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode2017.Solver
+{
+    internal partial class Day01 : BaseSolver
+    {
+        public override string PuzzleTitle { get; } = "Inverse Captcha";
+
+        public override string GetSolution1(bool isChallenge)
+        {
+            long result = 0;
+            foreach (string sequence in _puzzleInput)
+            {
+                for (int i = 0; i < sequence.Length; i++)
+                {
+                    result += sequence[i] == sequence[(i + 1) % sequence.Length] ? long.Parse(sequence[i].ToString()) : 0;
+                }
+            }
+            return result.ToString();
+        }
+
+        public override string GetSolution2(bool isChallenge)
+        {
+            long result = 0;
+            foreach (string sequence in _puzzleInput)
+            {
+                for (int i = 0; i < sequence.Length; i++)
+                {
+                    result += sequence[i] == sequence[(i + sequence.Length / 2) % sequence.Length] ? long.Parse(sequence[i].ToString()) : 0;
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
