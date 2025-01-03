e2206d2ab2fbf9d9457ddc01c0efbd9355fabd49
Fred <6550969+MagicFred888@users.noreply.github.com>
2025-01-03 13:38:07 +0100

    Solver for day 17

---
+++ AdventOfCode2017/Solver/Day17.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode2017.Solver
+{
+    internal partial class Day17 : BaseSolver
+    {
+        public override string PuzzleTitle { get; } = "Spinlock ";
+
+        private sealed class ChainedNode
+        {
+            public int Value { get; init; }
+            public ChainedNode Next;
+
+            public ChainedNode(int value)
+            {
+                Value = value;
+                Next = this;
+            }
+        }
+
+        public override string GetSolution1(bool isChallenge)
+        {
+            int finalBufferSize = 2017;
+            int steps = int.Parse(_puzzleInput[0]);
+
+            ChainedNode baseNode = new(0);
+            ChainedNode finalNode = PerformTheLoops(baseNode, steps, finalBufferSize);
+            return finalNode.Next.Value.ToString();
+        }
+
+        public override string GetSolution2(bool isChallenge)
+        {
+            int finalBufferSize = 50000000;
+            int steps = int.Parse(_puzzleInput[0]);
+
+            ChainedNode baseNode = new(0);
+            _ = PerformTheLoops(baseNode, steps, finalBufferSize);
+            return baseNode.Next.Value.ToString();
+        }
+
+        private static ChainedNode PerformTheLoops(ChainedNode baseNode, int steps, int finalBufferSize)
+        {
+            ChainedNode currentPosition = baseNode;
+            for (int virtualBufferSize = 1; virtualBufferSize <= finalBufferSize; virtualBufferSize++)
+            {
+                // Move to the next node
+                for (int i = 0; i < steps; i++)
+                {
+                    currentPosition = currentPosition.Next;
+                }
+
+                // Insert the new node
+                ChainedNode newNode = new(virtualBufferSize)
+                {
+                    Next = currentPosition.Next
+                };
+                currentPosition.Next = newNode;
+                currentPosition = newNode;
+            }
+            return currentPosition;
+        }
+    }
+}
